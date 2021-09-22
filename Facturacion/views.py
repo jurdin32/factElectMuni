1c0bdab38ae691f43ae65ce374ecc5736c3ce965
johnny <urdin32@gmail.com>
2021-09-22 16:09:53 -0500

    Cambios generales

--- Facturacion/views.py
+++ Facturacion/views.py
@@ -16,19 +16,16 @@ def index(request):
     pdf=""
     xml=""
     if request.GET.get('q'):
-        try:
-            archivo="FACTURA_%s"%request.GET.get('q')
-            xml = 'http://siim.elguabo.gob.ec:8080/recursos_siim/FE/autorizados/%s.xml'%archivo
-            myfile = requests.get(xml)
-            dic_xml = xmltodict.parse(myfile.content)
-            #print(dic_xml)
-            documento=xmltodict.parse(dic_xml['autorizacion']['comprobante'])
-            open('./static/'+archivo+'.xml', 'wb').write(myfile.content)
-            pdf = 'http://siim.elguabo.gob.ec:8080/recursos_siim/FE/autorizados/%s.pdf' % archivo
-            myfile = requests.get(pdf)
-            open('./static/'+archivo+'.pdf', 'wb').write(myfile.content)
-        except:
-            pass
+        archivo="FACTURA_%s"%request.GET.get('q')
+        xml = 'http://siim.elguabo.gob.ec:8080/recursos_siim/FE/autorizados/%s.xml'%archivo
+        myfile = requests.get(xml)
+        dic_xml = xmltodict.parse(myfile.content)
+        #print(dic_xml)
+        documento=xmltodict.parse(dic_xml['autorizacion']['comprobante'])
+        open('./static/'+archivo+'.xml', 'wb').write(myfile.content)
+        pdf = 'http://siim.elguabo.gob.ec:8080/recursos_siim/FE/autorizados/%s.pdf' % archivo
+        myfile = requests.get(pdf)
+        open('./static/'+archivo+'.pdf', 'wb').write(myfile.content)
         try:
             Documentos.objects.get(clave_acceso=documento['factura']['infoTributaria']['claveAcceso'])
         except:
